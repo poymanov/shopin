d1f9382695afd0df7189f25b0fcf7874b56763dd
Nikolay <n.poymanov@gmail.com>
2016-09-16 22:44:25 +0300

    Создана миграция для товаров

---
+++ console/migrations/m160916_194034_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `products`.
+ */
+class m160916_194034_create_products_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('products', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'slug' => $this->string(),
+            'price' => $this->string(),
+            'image' => $this->string(),
+            'preview_text' => $this->text(),
+            'full_description' => $this->text()
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('products');
+    }
+}
