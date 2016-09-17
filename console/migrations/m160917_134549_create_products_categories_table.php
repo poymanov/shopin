f7a2992a656a0cfef0a8bd20197d65efcc7aa460
Nikolay <n.poymanov@gmail.com>
2016-09-17 16:55:32 +0300

    Добавлена миграция для связующей таблицы товары-категории

---
+++ console/migrations/m160917_134549_create_products_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `products_categories`.
+ */
+class m160917_134549_create_products_categories_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('products_categories', [
+            'id' => $this->primaryKey(),
+            'product_id' => $this->integer(11),
+            'category_id' => $this->integer(11),
+        ]);
+
+        $this->addForeignKey('fk_products_products_categories', 'products_categories', 'product_id', 'products', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_categories_products_categories', 'products_categories', 'category_id', 'categories', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk_products_products_categories', 'products_categories');
+        $this->dropForeignKey('fk_categories_products_categories', 'products_categories');
+        $this->dropTable('products_categories');
+    }
+}
