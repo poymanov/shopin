6fcf5820c03492e6b2724beca17627d9d1b48e7f
Nikolay <n.poymanov@gmail.com>
2016-09-18 17:02:53 +0300

    Добавлена верстка для страницы с ошибкой

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -6,22 +6,15 @@
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="container">
+    <div class="four">
+        <h3><?= Html::encode($this->title) ?></h3>
+        <p><?= nl2br(Html::encode($message)) ?></p>
+        <a href="<?=Url::home()?>" class="hvr-skew-backward">Back To Home</a>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
