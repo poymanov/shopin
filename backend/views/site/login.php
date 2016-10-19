3e684cf97390a81f5691fd4e9d132ce63eec830b
Nikolay <n.poymanov@gmail.com>
2016-10-19 22:19:17 +0300

    Добавлена форма авторизации в админке

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -21,18 +21,19 @@ $this->title = 'Login';
 
         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" placeholder="Email">
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Username'])->label(false) ?>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password">
+                <?= $form->field($model, 'password')->passwordInput(['placeholder' => "Password"])->label(false) ?>
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="hidden" name="LoginForm[rememberMe]" value="0">
+                            <input type="checkbox" name="LoginForm[rememberMe]" value="1"> Remember Me
                         </label>
                     </div>
                 </div>
